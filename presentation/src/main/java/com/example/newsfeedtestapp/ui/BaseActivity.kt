58dec825dcdbe48e812a9daea4c0d19fbd3c4bfe
Fadel <mohamedfadel91@gmail.com>
2021-02-19 10:51:33 +0200

    the base activity for implementing dagger without boilerplate code

---
+++ presentation/src/main/java/com/example/newsfeedtestapp/ui/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsfeedtestapp.ui
+
+import dagger.android.support.DaggerAppCompatActivity
+
+/**
+ * Created by Mohamed Fadel
+ * Date: 1/31/2020.
+ * email: mohamedfadel91@gmail.com.
+ */
+abstract class BaseActivity : DaggerAppCompatActivity() {
+
+}
