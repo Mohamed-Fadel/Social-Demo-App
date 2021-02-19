cf52964ecac7db43b0902e638e3e0cf08b2c6d61
Fadel <mohamedfadel91@gmail.com>
2021-02-19 14:40:18 +0200

    * add network layer

---
+++ app/src/main/java/com/example/newsfeedtestapp/di/module/NetworkModule.kt
@@ -0,0 +1,45 @@
+package com.example.newsfeedtestapp.di.module
+
+import com.example.newsfeedtestapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+/**
+ * Created by Mohamed Fadel
+ * Date: 10/28/2019.
+ * email: mohamedfadel91@gmail.com.
+ */
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().apply {
+        connectTimeout(30, TimeUnit.SECONDS)
+        callTimeout(30, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }
+
+    }.build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+//        .addCallAdapterFactory(LiveDataCallAdapterFactory())
+        .build()
+}
